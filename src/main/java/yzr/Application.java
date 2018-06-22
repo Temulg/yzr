42bf11c27e6742633f8f36d909472604df231c52
Alex Dubov <oakad@yahoo.com>
2018-06-22 23:20:35 +1000

    Switch to gradle and plain java structure
    Signed-off-by: Alex Dubov <oakad@yahoo.com>

---
+++ src/main/java/yzr/Application.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018 Alex Dubov <oakad@yahoo.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package yzr;
+
+public class Application {
+	public static void main(String... args) {
+		System.out.format("Aaa\n");
+	}
+}
